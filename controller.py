38d1c828d5674403af6c7f67fc83fc494b202e40
thecheenee <acharya.shrinivas@gmail.com>
2024-02-12 13:28:26 +0530

    fixed view data

--- controller.py
+++ controller.py
@@ -13,8 +13,16 @@ def home():
     todo_query = Todo.query.filter_by(user_id=user_id)
     df = True
     items = []
+    view = {
+      'name': '',
+      'tasks': 0
+    }
     if todo_query:
       df = False
+      user_view_data = UserTodo.query.filter_by(userid=user_id).first()
+      if user_view_data:
+        view['name'] = user_view_data.name
+        view['tasks'] = int(user_view_data.tasks)
       for el in todo_query:
         time_data = datetime.strftime(datetime.fromtimestamp(float(el.created_on)),"%a %b %d, %Y at %H-%M-%S")
         items.append({
@@ -24,7 +32,7 @@ def home():
           'added':time_data
         })
       # print(items)
-    return render_template("index.html", login=True, username=name, items=items, empty=df)
+    return render_template("index.html", login=True, username=name, items=items, empty=df, user_data=view)
   else:
     return redirect("/login")
   
